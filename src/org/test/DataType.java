d770c47ec199f8252bbb1b7e538a72b37d186614
sarathy2511 <sarathy2511@gmail.com>
2020-10-23 18:45:14 +0530

    task done

---
+++ src/org/test/DataType.java
@@ -0,0 +1,19 @@
+package org.test;
+
+public class DataType {
+public static void main(String[] args) {
+	byte age = 21;
+	System.out.println("my age is :" +age);
+	
+	short id = 34;
+	System.out.println("my id is :" +id);
+	
+	 double salary = 611111.225;
+	 System.out.println("my sal is :" +salary);
+	float sal = 645422.45f;
+	 System.out.println("my sal is :" +sal);
+	
+	
+}
+
+}
